4ed47ea0a6c2af53fad8ea54d6ee3ce5c3c5ea28
JpVargass <136398972+JpVargass@users.noreply.github.com>
2023-07-07 21:59:35 -0400

    con Adapter

---
+++ app/src/main/java/cl/jpvs/modulo5ejercicio5/AdapterWords.java
@@ -0,0 +1,47 @@
+package cl.jpvs.modulo5ejercicio5;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import cl.jpvs.modulo5ejercicio5.databinding.ItemBinding;
+
+public class AdapterWords extends RecyclerView.Adapter<AdapterWords.ViewHolder> {
+    private List<String> words;
+    @NonNull
+    @Override
+    public AdapterWords.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemBinding binding = ItemBinding.inflate(LayoutInflater.from(parent.getContext()));
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterWords.ViewHolder holder, int position) {
+        String item = words.get(position);
+        holder.showInformation(item);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return words.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private ItemBinding itemBinding;
+
+        public ViewHolder(@NonNull ItemBinding binding) {
+            super(binding.getRoot());
+            itemBinding = binding;
+        }
+        public void showInformation (String words) {
+            itemBinding.textView.setText((words);
+
+        }
+    }
+}
